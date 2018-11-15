71cf8b45edc9f63443fd8decfdfd645c0f27dd69
Daniel Mann <raccoon2010@gmail.com>
2018-11-15 11:21:05 -0800

    made startActivity and it's layout

---
+++ app/src/main/java/com/example/durandal/budgetingmadesimple/StartActivity.java
@@ -0,0 +1,17 @@
+package com.example.durandal.budgetingmadesimple;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+    }
+}
