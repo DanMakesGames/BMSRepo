fad06416b69d776d54a9539be39a13a140fc3ebd
gabepizz <gabe@alohasunset.com>
2018-11-28 23:48:14 -0800

    buggy, yet there are things built

--- app/src/main/java/com/example/durandal/budgetingmadesimple/MainActivity.java
+++ app/src/main/java/com/example/durandal/budgetingmadesimple/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.durandal.budgetingmadesimple;
 
 import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
 import android.support.v4.content.res.TypedArrayUtils;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+
 import java.lang.Object;
 //import org.apache.commons.lang3.ArrayUtils;
 
@@ -34,11 +41,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private ListView expList;
     private Spinner timeDropdown;
     private Spinner catDropdown;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Code for navigation drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                }); // end of navbar code
+
 
         // Example code intended for developers, showing how to interact with the SQL database
         showcaseDatabase();
