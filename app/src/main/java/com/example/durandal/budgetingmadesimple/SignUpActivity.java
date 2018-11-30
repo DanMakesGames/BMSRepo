a0665933bb5b91443ea2b48f894b08b96bdb2b6f
Daniel Mann <raccoon2010@gmail.com>
2018-11-29 20:27:12 -0800

    Finished implementation, need testing

--- app/src/main/java/com/example/durandal/budgetingmadesimple/SignUpActivity.java
+++ app/src/main/java/com/example/durandal/budgetingmadesimple/SignUpActivity.java
@@ -1,9 +1,11 @@
 package com.example.durandal.budgetingmadesimple;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +14,12 @@ import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
     // TextFields
-    private EditText editFirstName;
-    private EditText editLastName;
     private EditText editUsername;
     private EditText editPassword;
     private EditText editConfirmPassword;
+    private EditText editSecretQuestion;
+    private EditText editSecretAnswer;
+    private EditText editEmail;
 
     // Buttons
     private Button signUpButton;
@@ -28,11 +31,12 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         // Link TextFields
-        editFirstName       = (EditText) findViewById(R.id.edit_firstName);
-        editLastName        = (EditText) findViewById(R.id.edit_lastName);
         editUsername        = (EditText) findViewById(R.id.edit_username);
         editPassword        = (EditText) findViewById(R.id.edit_password);
         editConfirmPassword = (EditText) findViewById(R.id.edit_confirmPassword);
+        editSecretQuestion = (EditText) findViewById(R.id.edit_secret_question);
+        editSecretAnswer = (EditText) findViewById(R.id.edit_secret_answer);
+        editEmail = (EditText) findViewById(R.id.edit_email);
 
         // Link Buttons
         signUpButton        = (Button)   findViewById(R.id.sign_up_button);
@@ -55,32 +59,24 @@ public class SignUpActivity extends AppCompatActivity {
                 // Set buttons and textFields as disabled
                 signUpButton.setEnabled(false);
                 backButton.setEnabled(false);
-                editFirstName.setEnabled(false);
-                editLastName.setEnabled(false);
                 editUsername.setEnabled(false);
                 editPassword.setEnabled(false);
                 editConfirmPassword.setEnabled(false);
+                editEmail.setEnabled(false);
+                editSecretAnswer.setEnabled(false);
+                editSecretQuestion.setEnabled(false);
+
 
                 // Get textField inputs
-                String firstName = editFirstName.getText().toString();
-                String lastName = editLastName.getText().toString();
                 String username = editUsername.getText().toString();
                 String password = editPassword.getText().toString();
                 String confirmPass = editConfirmPassword.getText().toString();
+                String email = editEmail.getText().toString();
+                String secretQuestion = editSecretQuestion.getText().toString();
+                String secretAnswer = editSecretAnswer.getText().toString();
 
                 // error flag
                 boolean error = false;
-
-                // Check for empty strings and valid strings
-                if (firstName.length() < 0 || !Pattern.matches("[a-zA-Z]+", firstName)) {
-                    editFirstName.setError("Invalid first name");
-                    error = true;
-                }
-
-                if (lastName.length() < 0 || !Pattern.matches("[a-zA-Z]+", lastName)) {
-                    editLastName.setError("Invalid last name");
-                    error = true;
-                }
 
                 if (username.length() < 0 || !Pattern.matches("[a-zA-Z]+", username)) {
                     editUsername.setError("Invalid username");
@@ -103,25 +99,67 @@ public class SignUpActivity extends AppCompatActivity {
                     error = true;
                 }
 
-                // Check valid username/doesn't already exist
+                if (email.length() < 0) {
+                    editPassword.setError("Should input email");
+                    error = true;
+                }
 
-                // Check for errors, reset fields
+                if (editSecretQuestion.length() < 0) {
+                    editPassword.setError("Should input question");
+                    error = true;
+                }
+
+                if (editSecretAnswer.length() < 0) {
+                    editPassword.setError("Should input answer");
+                    error = true;
+                }
+
+                // Check valid username/doesn't already exist
+                Cursor attemptCursor = BMSApplication.database.getUser(username);
+                if(attemptCursor.getCount() != 0) {
+                    editUsername.setError("Username is taken");
+                    error = true;
+                }
+
+                // Check for errors, reset fields, and return.
                 if (error) {
                     signUpButton.setEnabled(true);
                     backButton.setEnabled(true);
-                    editFirstName.setEnabled(true);
-                    editLastName.setEnabled(true);
+
                     editUsername.setEnabled(true);
                     editPassword.setEnabled(true);
                     editConfirmPassword.setEnabled(true);
 
+                    editEmail.setEnabled(true);
+                    editSecretQuestion.setEnabled(true);
+                    editSecretAnswer.setEnabled(true);
+
                     editPassword.setText("");
                     editConfirmPassword.setText("");
+                    return;
                 }
 
-                // Add to database
+                // Username and password combo are valid---
+                BMSApplication.database.createUser(username,password,email,secretQuestion,
+                        secretAnswer,0,0,0);
 
-                // Segue to HomeScreenActivity if no errors. Else return back to this page (SignUpActivity)
+                // now that user is created we need to setup account.
+                // get account
+                Cursor userCursor = BMSApplication.database.getUser(username);
+
+                // create instantiate account.
+                if (userCursor.getCount() == 0)
+                    Log.d("", "No data returned");
+                else{
+                    while (userCursor.moveToNext()) {
+
+                        BMSApplication.account = new UserAccount(
+                                Integer.parseInt(userCursor.getString(0)),
+                                userCursor.getString(1),
+                                userCursor.getString(3),
+                                userCursor.getString(2));
+                    }
+                }
             }
         }); // end signUpButton onClick
     }
