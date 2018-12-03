2591e2761836d3a40f8f728bb9bedc6bd5f0e405
Eric Wei <EricWei@Erics-MacBook-Pro-3.local>
2018-12-02 16:07:53 -0800

    D

--- app/src/main/java/com/example/durandal/budgetingmadesimple/MainActivity.java
+++ app/src/main/java/com/example/durandal/budgetingmadesimple/MainActivity.java
@@ -1,44 +1,31 @@
 package com.example.durandal.budgetingmadesimple;
 
 import android.content.Intent;
-import android.database.Cursor;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
-import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-
 import android.view.MenuItem;
-import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
-
-import java.lang.Object;
-//import org.apache.commons.lang3.ArrayUtils;
 
 import com.google.android.gms.common.util.ArrayUtils;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.time.Instant;
-import java.util.Arrays;
-
-
 
 import java.time.ZonedDateTime;
 import java.util.LinkedList;
 import java.util.List;
+
+//import org.apache.commons.lang3.ArrayUtils;
 
 //import org.apache.commons.lang3.ArrayUtils;
 
